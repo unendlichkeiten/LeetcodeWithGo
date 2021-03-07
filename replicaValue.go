dd966f9dbedbb49c404c716186787100349b7751
unendlichkeiten <canghaimingue@163.com>
2021-03-07 12:06:44 +0800

    add codingInterviewV2 Interview3Q2

--- replicaValue.go
+++ replicaValue.go
@@ -2,6 +2,8 @@ package leetCode
 
 import "errors"
 
+// 题目一：找出重复的数字
+// 方法二：hash表法
 func ReplicaValue_Hash(array []int)(int, error){
 	arrMap := make(map[int]int)
 
@@ -16,6 +18,8 @@ func ReplicaValue_Hash(array []int)(int, error){
 	return -1, errors.New("no value replica")
 }
 
+// 题目一：找出重复的数字
+// 方法二：扫描法
 func ReplicaValue_Scan(array []int)(int, error){
 	for key, value := range array {
 		compare:
@@ -33,3 +37,66 @@ func ReplicaValue_Scan(array []int)(int, error){
 
 	return -1, errors.New("no value replica")
 }
+
+// 获取枢轴
+func partition(num []int, left, right int) int {
+	pivot := num[left]
+	for ; left < right; {
+		for ; left < right && num[right] >= pivot; right-- {}
+		num[left] = num[right]
+		for ; left < right && num[left] <= pivot; left++ {}
+		num[right] = num[left]
+	}
+
+	num[left] = pivot
+	return left
+}
+
+// 快排
+func QuickSort2(num []int, left, right int) {
+	if left < right {
+		pivot := partition(num, left, right)
+		QuickSort2(num, left, pivot)
+		QuickSort2(num, pivot+1, right)
+	}
+}
+
+// 题目二：不修改数组找出重复数字
+// 方法一：空间换时间
+func RepicaValue_Mem(array []int) (int, error) {
+	tmpArray := make([]int, 0)
+	for _, value := range array {
+		tmpArray = append(tmpArray, value)
+	}
+
+	QuickSort2(tmpArray, 0, len(array)-1)
+
+	for i, value := range tmpArray{
+		if i != 0 && value == tmpArray[i-1] {
+			return value, nil
+		}
+	}
+	return -1, errors.New("not exists replica value")
+}
+
+// 题目二：不修改数组找出重复数字
+// 方法二：二分法
+func ReplicaValue_Div(array []int, left, right int) (int, error) {
+	for left < right {
+		m, n, i := (left+right)/2, (right-left)/2+1, 0
+		for key, value := range array {
+			if value <= m && key > 0 {
+				i++
+			}
+		}
+
+		if i > n {
+			right = m
+		} else {
+			left = m + 1
+		}
+	}
+
+	return left, nil
+}
+
--- replicaValue_test.go
+++ replicaValue_test.go
@@ -3,7 +3,7 @@ package leetCode
 import "testing"
 
 func TestReplicaValue_Hash(t *testing.T) {
-	testArray := []int{2, 3, 1, 0, 2, 5, 3}
+	testArray := []int{0, 3, 1, 0, 2, 5, 3}
 
 	if value, err := ReplicaValue_Hash(testArray); err == nil {
 		t.Logf("exists replica value %d", value)
@@ -11,9 +11,78 @@ func TestReplicaValue_Hash(t *testing.T) {
 }
 
 func TestReplicaValue_Scan(t *testing.T) {
-	testArray := []int{2, 3, 1, 0, 2, 5, 3}
+	testArray := []int{1, 3, 1, 0, 2, 5, 3}
 
 	if value, err := ReplicaValue_Scan(testArray); err == nil {
 		t.Logf("exists replica value %d", value)
+	}
+}
+
+func TestQuickSort2(t *testing.T) {
+	type args struct {
+		num   []int
+		left  int
+		right int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{"test01",args{[]int{3,6,2,3,2,5,1,9},0,7}},
+		{"test02",args{[]int{1,2,3,4,5,3,2,1},0,7}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			QuickSort2(tt.args.num, tt.args.left, tt.args.right)
+			t.Log(tt.args.num)
+		})
+	}
+}
+
+func TestRepicaValue_Mem(t *testing.T) {
+	type args struct {
+		array []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr error
+	}{
+		// TODO: Add test cases.
+		{"test01",args{[]int{3,6,2,3,2,5,1,9}}, 2, nil},
+		{"test02",args{[]int{1,2,3,4,5,3,2,1}}, 1, nil},
+	}
+	for _, tt := range tests {
+		got, err := RepicaValue_Mem(tt.args.array)
+		if got != tt.want || err != nil {
+			t.Errorf("RepicaValue_Mem() got = %v, want %v", got, tt.want)
+		}
+	}
+}
+
+func TestReplicaValue_Div(t *testing.T) {
+	type args struct {
+		array []int
+		left  int
+		right int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr error
+	}{
+		// TODO: Add test cases.
+		{"test01",args{[]int{0,3,6,2,3,2,5,1,9}, 1,8}, 2, nil},
+		{"test02",args{[]int{0,1,2,3,4,5,3,2,1}, 1,8}, 1, nil},
+		{"test03",args{[]int{0,1,1,1,2,2,2,2,2}, 1,8}, 1, nil},
+	}
+	for _, tt := range tests {
+			got, err := ReplicaValue_Div(tt.args.array, tt.args.left, tt.args.right)
+			if got != tt.want || err != nil {
+				t.Errorf("ReplicaValue_Div() got = %v, want %v", got, tt.want)
+			}
 	}
 }
