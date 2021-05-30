460bb7666f310a786202a09cdb573044be7f0f33
unendlichkeiten <79490404+unendlichkeiten@users.noreply.github.com>
2021-05-30 09:38:43 +0800

    feat( _076_minimum_window_substring) add practice

--- _076_minimum_window_substring/minimum_window_substring.go
+++ _076_minimum_window_substring/minimum_window_substring.go
@@ -3,6 +3,56 @@ package _076_minimum_window_substring
 import "math"
 
 func minWindow(s string, t string) string {
+	tMap, tCntInWinMap := map[byte]int{}, map[byte]int{}
+	for i := 0; i < len(t); i++ {
+		tMap[t[i]]++
+	}
+
+	sLen := len(s)
+	minWinLen := math.MaxInt16
+	minWinStart, minWinEnd := -1, -1
+
+	// 检查窗口是否包含所有元素
+	check := func() bool {
+		for k, v := range tMap {
+			if tCntInWinMap[k] < v {
+				return false
+			}
+		}
+		return true
+	}
+
+	for left, right := 0, 0; right < sLen; right++ {
+		// 计算子串元素出现的频次
+		if right < sLen && tMap[s[right]] > 0 {
+			tCntInWinMap[s[right]]++
+		}
+
+		// 窗口中包含子串所有元素且 left <= right (指针左移)
+		for check() && left <= right {
+			if right-left+1 < minWinLen {
+				minWinLen = right-left+1
+				minWinStart = left
+				minWinEnd = right
+			}
+
+			// 如果左移元素为子串中出现的元素
+			// 减去子串元素在窗口中出现的频次
+			if _, ok := tMap[s[left]]; ok {
+				tCntInWinMap[s[left]]--
+			}
+			left++
+		}
+	}
+
+	if minWinStart < 0 {
+		return ""
+	}
+
+	return s[minWinStart:minWinEnd+1]
+}
+
+func minWindowAns(s string, t string) string {
 	ori, cnt := map[byte]int{}, map[byte]int{}
 	for i := 0; i < len(t); i++ {
 		ori[t[i]]++
