280124f22864f88880b33c9df8c932808af13607
unendlichkeiten <canghaimingue@163.com>
2021-07-01 22:03:57 +0800

    feat(_417_pacific_atlantic_water_flow) add leetcode 417 test demo

--- _417_pacific_atlantic_water_flow/waterFlow_test.go
+++ _417_pacific_atlantic_water_flow/waterFlow_test.go
@@ -42,3 +42,42 @@ func Test_pacificAtlantic(t *testing.T) {
 		})
 	}
 }
+
+func Test_pacificAtlantic0(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"test01",
+			args{
+				[][]int{
+					{1, 2, 2, 3, 5},
+					{3, 2, 3, 4, 4},
+					{2, 4, 5, 3, 1},
+					{6, 7, 1, 4, 5},
+					{5, 1, 1, 2, 4},
+				},
+			},
+			[][]int{
+				{0, 4},
+				{1, 3},
+				{1, 4},
+				{2, 2},
+				{3, 0},
+				{3, 1},
+				{4, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pacificAtlantic0(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pacificAtlantic0() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
