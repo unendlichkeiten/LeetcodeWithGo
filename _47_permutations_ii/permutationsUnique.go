655a03dbfbba1804d7652d708718284d0ed9a755
unendlichkeiten <canghaimingue@163.com>
2021-07-04 21:54:02 +0800

    feat(_47_permutations_ii) add leetcode 47

---
+++ _47_permutations_ii/permutationsUnique.go
@@ -0,0 +1,49 @@
+package _47_permutations_ii
+
+import (
+	"sort"
+)
+
+func PermuteUnique(nums []int) [][]int {
+
+	// nums sorting
+	sort.Ints(nums)
+
+	res, track, used := make([][]int, 0), make([]int, 0), make([]bool, len(nums))
+	backtrace(nums, track, &res, &used)
+
+	return res
+}
+
+func backtrace(nums, track []int, res *[][]int, used *[]bool) {
+	if len(track) == len(nums) {
+		tmp := make([]int, len(track))
+		copy(tmp, track)
+		*res = append(*res, tmp)
+
+		return
+	}
+
+	for i, num := range nums {
+		if trimBranch(nums, i, *used) {
+			continue
+		}
+
+		if (*used)[i] == false {
+			(*used)[i] = true
+			track = append(track, num)
+			backtrace(nums, track, res, used)
+			track = track[:len(track)-1]
+			(*used)[i] = false
+		}
+	}
+}
+
+func trimBranch(nums []int, index int, used []bool) bool {
+	if index > 0 &&
+		nums[index] == nums[index-1] &&
+		used[index-1] == false {
+		return true
+	}
+	return false
+}
---
+++ _47_permutations_ii/permutationsUnique_test.go
@@ -0,0 +1,42 @@
+package _47_permutations_ii
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPermuteUnique(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"test01",
+			args{[]int{2, 1, 1}},
+			[][]int{
+				{1, 1, 2},
+				{1, 2, 1},
+				{2, 1, 1},
+			}},
+		{"test02",
+			args{[]int{2, 2, 1, 1}},
+			[][]int{
+				{1, 1, 2, 2},
+				{1, 2, 1, 2},
+				{1, 2, 2, 1},
+				{2, 1, 1, 2},
+				{2, 1, 2, 1},
+				{2, 2, 1, 1},
+			}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PermuteUnique(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PermuteUnique() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
