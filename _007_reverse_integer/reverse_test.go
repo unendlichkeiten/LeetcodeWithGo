b288a8561f174163d32be53a4efff5952bb24cf8
haiming_zhu <haiming_zhu@intsig.net>
2021-07-23 14:42:07 +0800

    feat(_007_reverse_integer): add leetcode 007

---
+++ _007_reverse_integer/reverse_test.go
@@ -0,0 +1,25 @@
+package _007_reverse_integer
+
+import "testing"
+
+func Test_reverse(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test-01", args{123}, 321},
+		{"test-02", args{-123}, -321},
+		{"test-02", args{0}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverse(tt.args.x); got != tt.want {
+				t.Errorf("reverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
