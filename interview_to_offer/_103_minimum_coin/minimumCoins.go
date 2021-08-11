7db1c579fd151e2aef1345fdae24505cc6eb3728
unendlichkeiten <canghaimingue@163.com>
2021-08-11 22:58:16 +0800

    fix(interview_to_offer) add 103

--- interview_to_offer/_103_minimum_coin/minimumCoins.go
+++ interview_to_offer/_103_minimum_coin/minimumCoins.go
@@ -1,5 +1,7 @@
 package _103_minimum_coin
 
+import "math"
+
 func coinChange(coins []int, amount int) int {
 	// 构建状态转移方程
 	// F(X) = MIN(F(X-coins[0]), F(X-coins[1]), ... F(X-coins[len(coins)-1])) + 1
@@ -7,11 +9,20 @@ func coinChange(coins []int, amount int) int {
 		return 0
 	}
 
-	minCoins := make(map[int]int)
-	for _, coin := range coins {
-		if amount > coin {
-			minCoins[amount] = min(coinChange(coins, amount-coin), minCoins[amount])
+	minCoins := make([]int, amount)
+	for i := 1; i <= amount; i++ {
+		minCoins[i] = math.MaxInt32
+		for j := 0; j < len(coins); j++ {
+			if i >= coins[j] &&
+				minCoins[i-coins[j]] != math.MaxInt32 &&
+				minCoins[i-coins[j]]+1 < minCoins[i] {
+				minCoins[i] = minCoins[i-coins[j]] + 1
+			}
 		}
+	}
+
+	if minCoins[amount] == math.MaxInt32 {
+		return -1
 	}
 
 	return minCoins[amount]
