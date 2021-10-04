7853896d18a21dbb5f0629a064412556d0157285
unendlichkeiten <canghaimingue@163.com>
2021-10-04 09:23:33 +0800

    docs(_058_length_of_last_word): add resolution analysis

--- _482_license_key_formatting/licenseKeyFormatting.go
+++ _482_license_key_formatting/licenseKeyFormatting.go
@@ -1,5 +1,13 @@
 package _482_license_key_formatting
 
+/*
+** 解题思路：
+** 1. 计算字符串中字符数
+** 2. 计算第一个分组字符数
+** 3. 遍历原字符串，若为小写字符，转为大写，若为分隔符跳过
+** 4. 将字符加入目标字符串中，每个分组结束追加字符‘-’
+ */
+
 func licenseKeyFormatting(s string, k int) string {
 	// 计算字符串中字符数
 	var cnt int
