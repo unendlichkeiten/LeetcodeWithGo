5a28fac40ad4800f048e440e986e914149521cba
unendlichkeiten <canghaimingue@163.com>
2022-10-27 00:24:16 +0800

    feat(_021_merge_two_sorted_lists): add leetcode 21

---
+++ _021_merge_two_sorted_lists/merge_two_lists.go
@@ -0,0 +1,52 @@
+package _021_merge_two_sorted_lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	newList, newListTail := (*ListNode)(nil), (*ListNode)(nil)
+	for list1 != nil && list2 != nil {
+		// 确定头节点
+		if newList == nil {
+			if list1.Val <= list2.Val {
+				newList, newListTail = list1, list1
+				list1 = list1.Next
+			} else {
+				newList, newListTail = list2, list2
+				list2 = list2.Next
+			}
+			continue
+		} else {
+			if list1.Val <= list2.Val {
+				newListTail.Next = list1
+				list1 = list1.Next
+				newListTail = newListTail.Next
+			} else {
+				newListTail.Next = list2
+				list2 = list2.Next
+				newListTail = newListTail.Next
+			}
+			newListTail.Next = nil
+		}
+	}
+
+	if list1 != nil {
+		newListTail.Next = list1
+	}
+
+	if list2 != nil {
+		newListTail.Next = list2
+	}
+
+	return newList
+}
