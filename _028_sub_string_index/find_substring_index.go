b095c08b367923e5bf20f2dd2047439c2f7e934f
unendlichkeiten <canghaimingue@163.com>
2022-11-11 08:05:35 +0800

    refactor(leetCodeWithGO): add leetcode 028

--- _028_sub_string_index/find_substring_index.go
+++ _028_sub_string_index/find_substring_index.go
@@ -1,10 +1,10 @@
 package _028_sub_string_index
 
 // 模式匹配
-func strStr(haystack string, needle string) int {
+func strStr(haystack string, src string) int {
 	i, j := 0, 0
-	for i < len(haystack) && j < len(needle) {
-		if haystack[i] == needle[j] {
+	for i < len(haystack) && j < len(src) {
+		if haystack[i] == src[j] {
 			i++
 			j++
 		} else {
@@ -13,7 +13,7 @@ func strStr(haystack string, needle string) int {
 		}
 	}
 
-	if j == len(needle) {
+	if j == len(src) {
 		return i - j
 	}
 
@@ -21,6 +21,38 @@ func strStr(haystack string, needle string) int {
 }
 
 // KMP 算法
-func strStrV2(haystack string, needle string) int {
-	return 0
+func strStrV2(haystack string, src string) int {
+	i, j, next := 0, 0, getNext(src)
+	for i < len(haystack) && j < len(src) {
+		if j == -1 || haystack[i] == src[j] {
+			i++
+			j++
+		} else {
+			j = next[j]
+		}
+	}
+
+	if j == len(src) {
+		return i - j
+	}
+
+	return -1
+}
+
+func getNext(src string) []int {
+	next := make([]int, len(src))
+	next[0] = -1         // 如果第一位失配，直接匹配主串的下一位
+	i, j := 0, -1        // i 表示子串下表，j 表示当前的 next[i] 所对应回退的步数
+	for i < len(src)-1 { // i 取值范围为 0~len(needle)-2 ,因为进入 if 判断后 i 会自增
+		if j == -1 /*第一位匹配失败*/ || src[j] == src[i] {
+			// 上面的比较仍然是假定失配位的前一位，因为当条件满足时指针会向后移动
+			i++
+			j++ // j 表示当前的 next[i] 所对应的索引
+			next[i] = j
+		} else {
+			j = next[j] // 指针回退
+		}
+	}
+
+	return next
 }
--- _028_sub_string_index/find_substring_index_test.go
+++ _028_sub_string_index/find_substring_index_test.go
@@ -1,6 +1,9 @@
 package _028_sub_string_index
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_strStr(t *testing.T) {
 	type args struct {
@@ -18,8 +21,28 @@ func Test_strStr(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+			if got := strStrV2(tt.args.haystack, tt.args.needle); got != tt.want {
 				t.Errorf("strStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getNext(t *testing.T) {
+	type args struct {
+		src string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test_01", args{"abcac"}, []int{-1, 0, 0, 0, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getNext(tt.args.src); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getNext() = %v, want %v", got, tt.want)
 			}
 		})
 	}
